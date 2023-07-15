ac11dd70bfc08b2b5cf1bf34383a709d0f05ce25
programmersBrain <137292577+programmersBrain@users.noreply.github.com>
2023-07-15 06:17:27 -0300

    10 Hover effects

---
+++ script.js
@@ -0,0 +1,8 @@
+/**
+ * ALL CREDIT GOES TO
+ * Craig Buckler
+ * http://www.sitepoint.com/css3-starwars-scrolling-text/
+ *
+ * Blame me for the music via embedded video bit.
+ * Aug 2015 - ...which probably doesn't work anymore and likely hasn't for some time...
+ */
